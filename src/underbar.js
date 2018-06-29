4b6aa2aba7a0fe16de6c495a5e974b523da5b15e
Faris Habib <faris.habib.7@gmail.com>
2018-06-28 18:40:06 -0700

    Added extend

--- src/underbar.js
+++ src/underbar.js
@@ -234,7 +234,7 @@
       }
         return acc;
     },false)
-    
+
   };
 
 
@@ -257,6 +257,17 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
+    
+    var answer = arguments[0];
+    var objs = Array.prototype.slice.call(arguments,1);
+
+      _.each(objs, function(item){
+        _.each(item,function(value,key){
+          answer[key]= value;
+        })
+      })
+
+    return answer;
   };
 
   // Like extend, but doesn't ever overwrite a key that already
